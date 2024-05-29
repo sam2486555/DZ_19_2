7a82be2beab38356737f83aef558ae09eec72e4e
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-05-29 15:51:46 +0300

    add settings in admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from catalog.models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "price", "category")
+    list_filter = ("category",)
+    search_fields = ("title", "desk",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
