ce317ddf5bf43197d926831ff8387f1e1ca93b0a
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-06-25 19:07:21 +0300

    change urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ContactsView, ProductsListview, ProductsDetailview, BlogCreateView, BlogUpdateView, \
-    BlogListview, BlogDetailview, BlogDeleteView
+from catalog.views import ContactsView, BlogCreateView, BlogUpdateView, \
+    BlogListview, BlogDetailview, BlogDeleteView, \
+    ProductListview, ProductDetailview, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('', ProductsListview.as_view(), name='products_list'),
-    path('products/<int:pk>/', ProductsDetailview.as_view(), name='products_detail'),
+    path('', ProductListview.as_view(), name='product_list'),
+    path('products/<int:pk>/', ProductDetailview.as_view(), name='product_detail'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('blog/', BlogListview.as_view(), name='blog_list'),
     path('blog/<int:pk>/', BlogDetailview.as_view(), name='blog_detail'),
     path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
