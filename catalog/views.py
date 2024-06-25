7bd74160ff950620dfa320f259b4e5705547173f
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-06-25 19:07:11 +0300

    change views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 from pytils.translit import slugify
@@ -6,13 +6,12 @@ from pytils.translit import slugify
 from catalog.models import Product, Blog
 
 
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
 
 
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
-
 
     def post(self, request):
         if request.method == 'POST':
@@ -24,34 +23,38 @@ class ContactsView(TemplateView):
         return render(request, template_name='catalog/contacts.html')
 
 
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'{name} {phone} {message}')
-#     return render(request, 'contacts.html')
-
-class ProductsListview(ListView):
+class ProductListview(ListView):
     model = Product
 
 
-# def products_list(request):
-#     products = Product.objects.all()
-#     context = {"products": products}
-#     return render(request, 'product_list.html', context)
-
-class ProductsDetailview(DetailView):
+class ProductDetailview(DetailView):
     model = Product
 
-# def products_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {"product": product}
-#     return render(request, 'products_detail.html', context)
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('title', 'desk', 'image', 'category', 'price', 'created_at', 'updated_at')
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('title', 'desk', 'image', 'category', 'price', 'created_at', 'updated_at')
+
+    def get_success_url(self):
+        return reverse('catalog:product_detail', args=(self.kwargs.get('pk'),))
+
+
+class ProductDeleteView(DeleteView):
+    template_name = 'catalog/product_delete.html'
+    model = Product
+    success_url = reverse_lazy('catalog:product_list')
+
 
 class BlogListview(ListView):
     template_name = 'blog/blog_list.html'
     model = Blog
+
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset(*args, **kwargs)
         queryset = queryset.filter(published=True)
