7be57a97e467393629ca59fdba19b3abc03f5d3d
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-07-08 00:28:53 +0300

    add users views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+import secrets
+import random
+import string
 
-# Create your views here.
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+
+from config.settings import EMAIL_HOST_USER
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Перейди по ссылке для подтверждения почты {url}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+
+        return super().form_valid(form)
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse('users:login'))
+
+def generate_password():
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(secrets.choice(password_characters) for _ in range(10))
+    return password
+
+
+def reset_password(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user = get_object_or_404(User, email=email)
+        password = generate_password()
+        user.set_password(password)
+        user.save()
+        send_mail(
+            subject='Восстановление пароля',
+            message=f'Ваш пароль успешно изменен: {password}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return redirect(reverse('users:login'))
+    return render(request, 'reset_password.html')
